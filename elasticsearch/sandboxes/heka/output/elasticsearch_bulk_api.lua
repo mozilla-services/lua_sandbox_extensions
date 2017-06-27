e3b63038232970238cf4123badf4dfc151517767
Mike Trinkala <trink@acm.org>
2017-06-27 13:38:01 -0700

    Fix the data loss in the ElasticSearch output when ES is down

--- elasticsearch/sandboxes/heka/output/elasticsearch_bulk_api.lua
+++ elasticsearch/sandboxes/heka/output/elasticsearch_bulk_api.lua
@@ -14,15 +14,14 @@ memory_limit    = 200e6
 
 address             = "127.0.0.1"
 port                = 9200
-timeout             = 10
+timeout             = 10    -- socket timeout
 flush_count         = 50000
 flush_on_shutdown   = false
--- preserve_data       = false -- we don't rely on preserve data anymore
-max_retry           = 0
-discard_on_error    = false -- discard the batch after max_retry+1 failed attemps to send the batch
-abort_on_error      = false -- stop this plugin after max_retry+1 failed attemps to send the batch
--- when setting abort_on_error=true, consider also settings shutdown_on_terminate or remove_checkpoints_on_terminate
-
+preserve_data       = false -- there is no state maintained in this plugin
+max_retry           = 0     -- number of seconds (retries once per second)
+discard_on_error    = false -- discard the batch after max_retry + 1 failed attempts to send the batch
+abort_on_error      = false -- stop this plugin after max_retry + 1 failed attempts to send the batch
+-- when setting abort_on_error = true, consider also settings shutdown_on_terminate or remove_checkpoints_on_terminate
 
 -- See the elasticsearch module directory for the various encoders and configuration documentation.
 encoder_module  = "encoders.elasticsearch.payload"
@@ -37,16 +36,17 @@ encoders_elasticsearch_common    = {
 require "table"
 require "rjson"
 require "string"
-local ltn12     = require "ltn12"
-local time      = require "os".time
-local socket    = require "socket"
-local http      = require("socket.http")
-local address   = read_config("address") or "127.0.0.1"
-local port      = read_config("port") or 9200
-local timeout   = read_config("timeout") or 10
-local discard   = read_config("discard_on_error")
-local abort     = read_config("abort_on_error")
-local maxretry  = read_config("max_retry") or 0
+local ltn12         = require "ltn12"
+local time          = require "os".time
+local socket        = require "socket"
+local http          = require("socket.http")
+local address       = read_config("address") or "127.0.0.1"
+local port          = read_config("port") or 9200
+local timeout       = read_config("timeout") or 10
+local discard       = read_config("discard_on_error")
+local abort         = read_config("abort_on_error")
+local max_retry     = read_config("max_retry") or 0
+assert(not (abort and discard), "abort_on_error and discard_on_error are mutually exclusive")
 
 local encoder_module = read_config("encoder_module") or "encoders.elasticsearch.payload"
 local encode = require(encoder_module).encode
@@ -58,7 +58,7 @@ local batch_file        = string.format("%s/%s.batch", read_config("output_path"
 local flush_on_shutdown = read_config("flush_on_shutdown")
 local ticker_interval   = read_config("ticker_interval")
 local flush_count       = read_config("flush_count") or 50000
-local last_flush        = time()
+assert(flush_count > 0, "flush_count must be greater than zero")
 
 local client
 local function create_client()
@@ -81,6 +81,10 @@ local req_headers = {
 }
 
 local function send_request() -- hand coded since socket.http doesn't support keep-alive connections
+    if not client then client, err = pcreate_client() end
+    if err then print(err); return false; end
+
+    local success = true
     local fh = assert(io.open(batch_file, "r"))
     req_headers["content-length"] = fh:seek("end")
     client:sendrequestline("POST", "/_bulk")
@@ -94,7 +98,6 @@ local function send_request() -- hand coded since socket.http doesn't support ke
         code = client:receivestatusline()
     end
     headers = client:receiveheaders()
-    local ok, err, ret = true, nil, 0
     if code ~= 204 and code ~= 304 and not (code >= 100 and code < 200) then
         if code == 200 and string.match(headers["content-type"], "^application/json") then
             local body = {}
@@ -104,20 +107,20 @@ local function send_request() -- hand coded since socket.http doesn't support ke
             local ok, doc = pcall(rjson.parse, response)
             if ok then
                 if doc:value(doc:find("errors")) then
-                    ret = -1
-                    err = string.format("ElasticSearch server reported errors processing the submission")
+                    print(string.format("ElasticSearch server reported errors processing the submission, not all messages were indexed"))
+                    -- todo track partial batch failure counts https://github.com/mozilla-services/lua_sandbox_extensions/issues/89
+                    -- the partial failure is most likely due to bad input, so no retry is attempted as it would just fail again
                 end
             else
-                ret = -3
-                err = string.format("HTTP response didn't contain valid JSON. err: %s", doc)
+                print(string.format("HTTP response didn't contain valid JSON. err: %s", doc))
             end
         else
             client:receivebody(headers, ltn12.sink.null())
         end
 
-        if not err and code > 304 then
-            ret = -1
-            err = string.format("HTTP response error. Status: %d", code)
+        if code > 304 then
+            success = false
+            print(string.format("HTTP response error. Status: %d", code))
         end
     end
 
@@ -126,81 +129,72 @@ local function send_request() -- hand coded since socket.http doesn't support ke
         client = nil
     end
 
-    return true, err, ret
+    return success
 end
-local psend_request = socket.protect(function(client) return send_request(client) end)
+local psend_request = socket.protect(function() return send_request() end)
 
 
+local send_on_start = false
+local last_flush    = time()
+local batch_count   = 0
+local retry_count   = 0
 local batch = assert(io.open(batch_file, "a+"))
-local function send_batch()
-    local err
-    if not client then
-        client, err = pcreate_client()
-    end
-    if err then return -3, err end -- retry indefinitely
-
-    batch:flush()
-    local ok, err, ret = psend_request(client)
-    if not ok then -- network error
-        client = nil
-        return -3, err
-    end
-    last_flush = time()
-    return ret, err
-end
-
-local batch_count = 0
-local retry       = false
-local try_count   = 0
-
-for _ in io.lines(batch_file) do
+for _ in io.lines(batch_file) do  -- ensure we have a correct count when resuming after an abort
     batch_count = batch_count + 1
 end
 batch_count = batch_count / 2
 if batch_count >= flush_count then
-    retry = true
+    send_on_start = true
 end
 
-local function send_batch2()
-    try_count = try_count + 1
-    local ret, err = send_batch()
-    if ret < 0 then client = nil end -- always use a new connection after an error
-
-    if ret == 0 then
-        retry = false
-    elseif discard and try_count > maxretry then
-        retry = false
-        err = string.format("Discarding %d messages after %d attemps, ret=%d err=%s", batch_count, try_count, ret, err)
-        ret = -1
-    elseif abort and try_count > maxretry then
-        error(string.format("Abort sending %d messages after %d attemps, ret=%d err=%s", batch_count, try_count, ret, err))
-    else
-        retry = true
-        err = string.format("Error sending %d messages, attemps nb %d, ret=%d err=%s", batch_count, try_count, ret, err)
-        ret = -3
-    end
-
-    if not retry then
-        batch_count = 0
-        try_count   = 0
-        batch:close()
-        batch = assert(io.open(batch_file, "w"))
-    end
-
-    return ret, err
+local function finalize_batch()
+    last_flush  = time()
+    batch_count = 0
+    retry_count = 0
+    batch:close()
+    batch = assert(io.open(batch_file, "w"))
 end
+
+local function send_batch()
+    batch:flush()
+    local ok, err = psend_request()
+    if not ok then
+        if err then print(err) end
+        client = nil
+        retry_count = retry_count + 1
+        if discard and retry_count > max_retry then
+            print(string.format("discarded %d messages", batch_count))
+            finalize_batch()
+            return true
+        elseif abort and retry_count > max_retry then
+            error(string.format("Abort sending %d messages after %d attempts", batch_count, retry_count))
+        end
+        return false
+    end
+    finalize_batch()
+    return true
+end
+
 
 function process_message()
-    if not retry then
-        local ok, data = pcall(encode)
-        if not ok then return -1, data end
-        if not data then return -2 end
-        batch:write(data)
-        batch_count = batch_count + 1
+    if batch_count >= flush_count then -- attempt to transmit a failed batch before accepting new data
+        if not send_batch() then
+            return -3 -- retry until successful or it errors out
+        end
+        if not send_on_start then
+            return 0 -- break the retry loop and allow new data to start flowing again
+        end
+        send_on_start = false
     end
 
+    local ok, data = pcall(encode)
+    if not ok then return -1, data end
+    if not data then return -2 end
+    batch:write(data)
+    batch_count = batch_count + 1
+
     if batch_count >= flush_count then
-        return send_batch2()
+        send_batch()
     end
     return 0
 end
@@ -209,6 +203,6 @@ end
 function timer_event(ns, shutdown)
     local timedout = (ns / 1e9 - last_flush) >= ticker_interval
     if (timedout or (shutdown and flush_on_shutdown)) and batch_count > 0 then
-        send_batch2()
+        send_batch()
     end
 end
