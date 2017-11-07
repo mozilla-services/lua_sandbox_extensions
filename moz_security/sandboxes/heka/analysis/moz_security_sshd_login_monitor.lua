e831099b4ecc7f199e6b457684c00b3d5b47a86e
Mike Trinkala <trink@acm.org>
2017-11-07 14:08:33 -0800

    Update the bastion alert message to use the array schema
    - Remove the dependency on the date module
    - Add a unique id in the subject for PagerDuty
    - Remove the user map it will be managed in PagerDuty

--- moz_security/sandboxes/heka/analysis/moz_security_sshd_login_monitor.lua
+++ moz_security/sandboxes/heka/analysis/moz_security_sshd_login_monitor.lua
@@ -15,45 +15,31 @@ message_matcher = "Type == 'logging.shared.bastion.systemd.sshd' && Fields[sshd_
 ticker_interval = 0
 process_message_inject_limit = 1
 
-user_map = {
-    foobar = "foobar@pagerduty.com",
-    -- ["*"]     = "user_map_maintainer@pagerduty.com" -- catch all
-}
-
+-- default_email = "foxsec-alerts@mozilla.com"
 ```
 --]]
 require "string"
 
-local user_map = read_config("user_map") or error("user_map must be set")
-for k,v in pairs(user_map) do
-    user_map[k] = string.format("<%s>", v)
-end
-
-local default = user_map["*"]
-if default then
-    local mt = {__index = function(t, k) return default end }
-    setmetatable(user_map, mt);
-end
-
+local default_email = read_config("default_email") or "foxsec-alerts@mozilla.com"
 local msg = {
     Type = "alert",
     Payload = "",
     Severity = 1,
     Fields = {
-        id                      = "sshd",
-        summary                 = "",
-        ["email.recipients"]    = ""
+        {name = "id"                , value = "sshd"},
+        {name = "summary"           , value = ""},
+        {name = "email.recipients"  , value = {string.format("<%s>", default_email)}}
     }
 }
 
 function process_message()
-    local ip    = read_message("Fields[remote_addr]")
     local user  = read_message("Fields[remote_user]")
-    local email = user_map[user]
-    if not email then return -1, "no user mapping specified" end
+    local ip    = read_message("Fields[remote_addr]")
+    local id    = string.format("%X%X%X%X%X%X%X%X%X%X%X%X%X%X%X%X",
+                                string.byte(read_message("Uuid"), 1, 16))
 
-    msg.Fields.summary = string.format("%s logged into bastion from %s", user, ip)
-    msg.Fields["email.recipients"] = email
+    msg.Fields[2].value    = string.format("%s logged into bastion from %s id:%s", user, ip, id)
+    msg.Fields[3].value[2] = string.format("<manatee-%s@moz-svc-ops.pagerduty.com>", user)
     inject_message(msg)
     return 0
 end
