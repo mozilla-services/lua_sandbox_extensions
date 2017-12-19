ec1de38ee4aaa2c3b97b9437a128704000a7583d
Mike Trinkala <trink@acm.org>
2017-12-19 12:59:28 -0800

    Issue 194 - Add topic configuration support to Kafka output

--- kafka/sandboxes/heka/output/kafka.lua
+++ kafka/sandboxes/heka/output/kafka.lua
@@ -25,6 +25,12 @@ producer_conf = {
     ["topic.metadata.refresh.interval.ms"] = -1,
 }
 
+-- https://github.com/edenhill/librdkafka/blob/master/CONFIGURATION.md#topic-configuration-properties
+topic_confs = {
+    -- <topic_name> = {<topic_conf>},
+    ["*"] = {<topic_conf>} -- optional default topic configuration
+}
+
 -- Specify a module that will encode/convert the Heka message into its output representation.
 encoder_module = "encoders.heka.protobuf" -- default
 ```
@@ -33,10 +39,20 @@ local brokerlist        = read_config("brokerlist") or error("brokerlist must be
 local topic_constant    = read_config("topic_constant")
 local topic_variable    = read_config("topic_variable") or "Logger"
 local producer_conf     = read_config("producer_conf")
+local topic_confs       = read_config("topic_confs") or {}
 local encoder_module    = read_config("encoder_module") or "encoders.heka.protobuf"
 local encode = require(encoder_module).encode
 if not encode then
     error(encoder_module .. " does not provide an encode function")
+end
+
+assert(type(topic_confs) == "table", "topic_confs must be a table")
+for k,v in pairs(topic_confs) do
+    assert(type(v) == "table", k .. " topic_conf must be a table")
+    if k == "*" then
+        local mt = {__index = function(t, k) return v end }
+        setmetatable(topic_confs, mt);
+    end
 end
 
 local producer = kafka.producer(brokerlist, producer_conf)
@@ -46,7 +62,7 @@ function process_message(sequence_id)
     if not topic then
         topic = read_message(topic_variable) or "unknown"
     end
-    producer:create_topic(topic) -- creates the topic if it does not exist
+    producer:create_topic(topic, topic_confs[topic]) -- creates the topic if it does not exist
 
     producer:poll()
     local ok, data = pcall(encode)
