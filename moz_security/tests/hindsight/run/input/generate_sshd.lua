f757900dd0b3fd858231a8edbe2e33efa44acb43
Aaron Meihm <ameihm@mozilla.com>
2018-01-15 14:30:54 -0600

    io_module to extend heka message with GeoIP information
    Intended to be used with other io_modules such as decoders; extends
    existing message with GeoIP information based on the supplied configuration
    which indicates that fields that should be processed in the message and
    the types of geolocation information that should be added.
    
    Resolves #190

--- moz_security/tests/hindsight/run/input/generate_sshd.lua
+++ moz_security/tests/hindsight/run/input/generate_sshd.lua
@@ -8,8 +8,11 @@
 
 
 local tests = {
-    {"11111111-1111-1111-1111-111111111111", "trink" , "192.168.1.1"},
+    {"11111111-1111-1111-1111-111111111111", "trink", "192.168.1.1"},
+    {"11111111-1111-1111-1111-111111111112", "trink", "192.168.1.2"}
 }
+
+ga = require "geoip.geo_append"
 
 local msg = {
     Timestamp = nil,
@@ -27,6 +30,7 @@ function process_message()
         msg.Uuid = v[1]
         msg.Fields.remote_user = v[2]
         msg.Fields.remote_addr = v[3]
+        msg = ga.geo_append(msg)
         inject_message(msg)
     end
     return 0
