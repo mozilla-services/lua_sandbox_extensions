da2636d0548b183a34f2d9c61b65e92947c2bc74
Aaron Meihm <ameihm@mozilla.com>
2018-02-01 11:05:19 -0600

    in sshd login monitor, config option to send to only default

--- moz_security/tests/hindsight/run/output/sshd_login_verification.lua
+++ moz_security/tests/hindsight/run/output/sshd_login_verification.lua
@@ -8,6 +8,7 @@
 
 require "string"
 
+-- Validate normal configuration, default email + manatee
 local results = {
     { summary    = "trink logged into bastion from 192.168.1.1",
       recipients = {"<foxsec-dump+OutOfHours@mozilla.com>", "<manatee-trink@moz-svc-ops.pagerduty.com>"}
@@ -17,13 +18,33 @@ local results = {
     },
 }
 
+-- Validate alternate configuration, modified default and no manatee append
+local results_only_default = {
+    { summary    = "trink logged into bastion from 192.168.1.1",
+      recipients = {"<foxsec-alternate+OutOfHours@mozilla.com>"}
+    },
+    { summary    = "trink logged into bastion from 192.168.1.2 (Mountain View, US)",
+      recipients = {"<foxsec-alternate+OutOfHours@mozilla.com>"}
+    },
+}
+
+local resset = {}
+if read_config("only_default_email") then
+    resset = results_only_default
+else
+    resset = results
+end
+
 local cnt = 0
+
+
 function process_message()
-    local summary = read_message("Fields[summary]") or "nil"
-    local dflt_recip = read_message("Fields[email.recipients]") or "nil"
-    local user_recip = read_message("Fields[email.recipients]", 0, 1) or "nil"
+    local summary = read_message("Fields[summary]") or error("no summary field")
+    local dflt_recip = read_message("Fields[email.recipients]") or error("no email recipients")
+    local user_recip = read_message("Fields[email.recipients]", 0, 1) or nil
+
     cnt = cnt + 1
-    local er = results[cnt]
+    local er = resset[cnt]
     assert(er, "too many messages")
 
     if er.summary ~= summary then
@@ -32,8 +53,11 @@ function process_message()
     if er.recipients[1] ~= dflt_recip then
         error(string.format("test:%d result:%s", cnt, dflt_recip))
     end
-    if er.recipients[2] ~= user_recip then
-        error(string.format("test:%d result:%s", cnt, user_recip))
+    if #er.recipients > 1 then
+        assert(er.recipients[2], "no user recipient in test case")
+        if er.recipients[2] ~= user_recip then
+            error(string.format("test:%d result:%s", cnt, user_recip))
+        end
     end
     return 0
 end
