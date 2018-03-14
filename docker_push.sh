a752a0fb191bf1130b1b30c23aad522f4615e71a
Aaron Meihm <ameihm@mozilla.com>
2018-03-14 12:39:55 -0500

    add travis deploy step

---
+++ docker_push.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+tag=""
+if [[ $1 = "master" ]]; then
+    tag="mozilla/lua_sandbox_extensions:master"
+elif [[ $1 = "dev" ]]; then
+    tag="mozilla/lua_sandbox_extensions:dev"
+else
+    exit 1
+fi
+
+docker tag mozilla/lua_sandbox_extensions $tag
+docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD"
+docker push $tag
