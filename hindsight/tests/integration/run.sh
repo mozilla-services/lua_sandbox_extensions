6861be92b4a66ff5b0785889aaacb193502ca4b7
Mike Trinkala <trink@acm.org>
2018-06-27 10:16:44 -0700

    Update the return code for the new hindsight_cli

--- hindsight/tests/integration/run.sh
+++ hindsight/tests/integration/run.sh
@@ -4,4 +4,4 @@ mkdir -p load/input
 mkdir -p load/analysis
 mkdir -p load/output
 hindsight_cli ../hindsight.cfg 7
-rc=$?; if [[ $rc != 6 ]]; then exit $rc; fi
+rc=$?; if [[ $rc != 4 ]]; then exit $rc; fi
