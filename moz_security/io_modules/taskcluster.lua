828677034ed7acadf0fa780bad319d8dde49e090
AJ Bahnken <1144310+ajvb@users.noreply.github.com>
2018-10-01 07:07:29 -0700

    Extract usernames from Taskcluster audit logs (#382)
    * Extract usernames from Taskcluster audit logs
    
    Adds the taskcluster#add_username func that can be used with another
    decoder to extract the ldap username from a taskcluster clientid. This
    makes plugging into other analysis sandboxes much simpler.
    
    * Move username field and matchers into config options
    
    * switch to using actual module name for configuration
    
    * user username_field within taskcluster add_username()
    
    * Fix example decoder config in taskcluster

---
+++ moz_security/io_modules/taskcluster.lua
@@ -0,0 +1,68 @@
+-- This Source Code Form is subject to the terms of the Mozilla Public
+-- License, v. 2.0. If a copy of the MPL was not distributed with this
+-- file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+--[[
+# Heka Message Extensions for Taskcluster
+
+This module is intended to be used with another IO module such as a decoder to
+add a username field if the client is a human user.
+
+## Functions
+
+### add_username
+
+Given a Heka message with a Taskcluster clientid, attempt to parse out the username
+associated with the clientid. This is currently specific to Mozilla LDAP usernames,
+as that is the primary way in which human users authenticate with Taskcluster. If no
+username is found, do nothing.
+
+*Arguments*
+- msg (table) - original message
+- field_name (string) - field name in the message to lookup
+
+*Return*
+- none - the message is modified in place if a username is found
+
+## Configuration examples
+taskcluster = {
+    username_field = "username",
+    matchers = {
+      "'mozilla-auth0/ad|Mozilla-LDAP|' (s <- {%w+}) '/'"
+    }
+}
+decoder_module = {
+    {
+        "decoders.heka.table_to_fields",
+        { clientid = "taskcluster#add_username"}
+    }
+}
+--]]
+
+local module_name = ...
+local cfg         = read_config(module_name) or error(module_name .. " configuration not found")
+assert(type(cfg.matchers) == "table", "matchers configuration must be a table")
+
+local username_field = cfg.username_field or "username"
+
+local re     = require "re"
+local ipairs = ipairs
+
+
+local M = {}
+setfenv(1, M)
+
+function add_username(msg, field_name)
+    if not msg.Fields then return end
+    local clientid = msg.Fields[field_name]
+
+    for _, matcher in ipairs(cfg.matchers) do
+      username = re.match(clientid, matcher)
+      if username then
+        msg.Fields[username_field] = username
+        break
+      end
+    end
+end
+
+return M
---
+++ moz_security/tests/taskcluster.lua
@@ -0,0 +1,34 @@
+-- This Source Code Form is subject to the terms of the Mozilla Public
+-- License, v. 2.0. If a copy of the MPL was not distributed with this
+-- file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+local cfg = {
+    taskcluster = {
+        username_field = "username",
+        matchers = {
+          "'mozilla-auth0/ad|Mozilla-LDAP|' (s <- {%w+}) '/'"
+        }
+    }
+}
+
+function read_config(k)
+    return cfg[k]
+end
+
+local taskcluster = require "taskcluster"
+local string = require "string"
+
+data = {
+  {{ Fields = { clientid = "no-user-name" } }, nil},
+  {{ Fields = { clientid = "mozilla-auth0/ad|Mozilla|almost/" } }, nil},
+  {{ Fields = { clientid = "mozilla-auth0/ad|Mozilla|almost/again" } }, nil},
+  {{ Fields = { clientid = "mozilla-auth0/ad|Mozilla-LDAP|picard/" } }, "picard"},
+  {{ Fields = { clientid = "mozilla-auth0/ad|Mozilla-LDAP|picard/test" } }, "picard"},
+}
+
+for i, msg in ipairs(data) do
+  taskcluster.add_username(msg[1], "clientid")
+
+  errmsg = string.format("test cnt:%d failed. %s != %s", i, tostring(msg[1].Fields["username"]), tostring(msg[2]))
+  assert(msg[1].Fields["username"] == msg[2], errmsg)
+end
