764b8eb11abcbe100366e06fb653f601d2abb4eb
Kai S <kai@xs4all.nl>
2019-07-11 18:14:07 +0200

    Sendmail (#442)
    * basic keyvalue grammar for sendmail logs
    
    It still has a few nits to solve.
    
    * Bump the version
    
    * PR fixes
    
    - add the space to the separator
    - remove the unnecessary spaces after key and value
    - make the sendmailid match better

---
+++ syslog/modules/lpeg/sendmail.lua
@@ -0,0 +1,30 @@
+-- This Source Code Form is subject to the terms of the Mozilla Public
+-- License, v. 2.0. If a copy of the MPL was not distributed with this
+-- file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+--[[
+# Sendmail module
+
+## Variables
+### LPEG Grammars
+* `grammar`
+--]]
+
+local l = require "lpeg"
+l.locale(l) -- we want l.alpha etc...
+
+local rawset = rawset
+
+local M = {}
+setfenv(1, M) -- Remove external access to contain everything in the module
+
+local sep = l.P", "
+local key = l.C(l.alpha^1)
+local value = l.C((l.P(1) - sep)^1)
+local pair = l.Cg(key * "=" * value) * sep^-1
+
+local sendmailid = l.Cg(l.alnum^-8 * l.digit^6, "sendmailid") * l.P": "
+
+grammar = l.Cf(l.Ct(sendmailid) * pair^0, rawset)
+
+return M
