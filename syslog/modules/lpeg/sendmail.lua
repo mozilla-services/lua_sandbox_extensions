f3b396791dbf6dfe343315cb72a4df1775f21086
Kai Storbeck <kai@xs4all.nl>
2019-07-30 15:12:31 +0200

    Untrust the stat= key; value can contain garbage

--- syslog/modules/lpeg/sendmail.lua
+++ syslog/modules/lpeg/sendmail.lua
@@ -21,7 +21,9 @@ setfenv(1, M) -- Remove external access to contain everything in the module
 local sep = l.P", "
 local key = l.C(l.alpha^1)
 local value = l.C((l.P(1) - sep)^1)
-local pair = l.Cg(key * "=" * value) * sep^-1
+local statpair = l.Cg(l.C(l.P'stat') * l.P'=' * l.C(l.P(1)^0))
+local normalpair = l.Cg(key * "=" * value) * sep^-1
+local pair = statpair + normalpair
 
 local sendmailid = l.Cg(l.alnum^-8 * l.digit^6, "sendmailid") * l.P": "
 
